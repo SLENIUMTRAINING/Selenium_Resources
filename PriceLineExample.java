51952d4dbb393ed262ed3dd84122af115fc19826
seleniyum <seleniyum@gmail.com>
2015-12-21 13:50:02 -0500

    Update PriceLineExample.java

--- PriceLineExample.java
+++ PriceLineExample.java
@@ -2,7 +2,11 @@ Learling WebDriver package drivers;
 
 import java.util.List;
 
-import org.openqa.selenium.By; import org.openqa.selenium.WebDriver; import org.openqa.selenium.WebElement; import org.openqa.selenium.firefox.FirefoxDriver; import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.By; 
+import org.openqa.selenium.WebDriver; 
+import org.openqa.selenium.WebElement; 
+import org.openqa.selenium.firefox.FirefoxDriver; 
+import org.openqa.selenium.support.ui.Select;
 
 public class WebD {
 
